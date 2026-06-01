afc59ef80dc97140a103a803ce2dd1cb64e2294e
Edward Baitsewe <edward@monatemedia.com>
2026-06-01 06:43:00 +0200

    Make formatDate fn and sort Articles bby latest first

---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+
+
+// Format publication dates for better readability
+function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return new Date(date).toLocaleDateString(undefined, options);
+}
+
+export { formatDate };
